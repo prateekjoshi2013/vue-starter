edbf3ec78c96bc51e0d9f2d423f7ae448ac4f101
Prateek Joshi <prateekjoshi2013@gmail.com>
2020-06-01 20:38:31 -0400

    added lifecycle hooks for directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,9 +1,23 @@
 /* eslint-disable no-param-reassign */
-export default {
-  bind: (element, binding) => {
-    Object.keys(binding.value).forEach((key) => {
-      element.style[key] = binding.value[key];
-    });
-    element.style.position = 'absolute';
-  },
-};
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((key) => {
+    element.style[key] = binding.value[key];
+  });
+  element.style.position = 'absolute';
+}
+
+// lifecycle hook
+// export default {
+//     bind: (element, binding) => {
+//       Object.keys(binding.value).forEach((key) => {
+//         element.style[key] = binding.value[key];
+//       });
+//       element.style.position = 'absolute';
+// },
+// bind: (element, binding) => {
+//     Object.keys(binding.value).forEach((key) => {
+//       element.style[key] = binding.value[key];
+//     });
+//     element.style.position = 'absolute';
+// },
+// }
